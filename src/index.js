18857239aeb9bb36d903dec520e36613ccc7d9b5
anastasiia-kolieva <nastya1110@gmail.com>
2020-10-06 21:16:08 +0300

    some corrections

--- src/index.js
+++ src/index.js
@@ -7,32 +7,41 @@ import templateForOneCountry from './templates/template-one-country.hbs';
 import refs from './js/refs.js';
 import './styles.css';
 
-refs.searchFormInput.addEventListener('input', debounce(debouncendFunctionOfSerching,500));
+refs.searchFormInput.addEventListener(
+  'input',
+  debounce(debouncendFunctionOfSerching, 500),
+);
 
 function debouncendFunctionOfSerching(event) {
-    event.preventDefault();
-  
-    // вызов функции результата поиска инпута перед вводом нового поиска страны. 
-    clearSearchFormInput();
-  
-    // значение которое вводят в инпут
-    const inputValue = refs.searchFormInput.value;
-  
-    fetchCountries(inputValue).then(countries => {
-      if (countries.length === 1) {
-        // если одна страна
-        buildMarkupForOneCountry(countries);
-      } else if (countries.length <= 10) {
-        // список стран (если от 2-х до 10-х стран)
-        buildMarkupForManyCountries(countries);
-      } else if (countries.length > 10) {
-        // если стран более чем 10 стран подошедших под критерий введенный пользователем, в интерфейсе отображается нотификация о том, что необходимо сделать запрос более специфичным.
-        error({
-          text: 'Too many matches found. Please enter a more specific query!',
-        });
-      }
-    });
-  }
+  event.preventDefault();
+
+  // вызов функции результата поиска инпута перед вводом нового поиска страны.
+  clearSearchFormInput();
+
+  // значение которое вводят в инпут
+  const inputValue = refs.searchFormInput.value;
+
+  fetchCountries(inputValue).then(countries => {
+
+    if (countries.length === 1) {
+      // если одна страна
+      buildMarkupForOneCountry(countries);
+    } else if (countries.length <= 10) {
+      // список стран (если от 2-х до 10-х стран)
+      buildMarkupForManyCountries(countries);
+    } else if (countries.length > 10) {
+      // если стран более чем 10 стран подошедших под критерий введенный пользователем, в интерфейсе отображается нотификация о том, что необходимо сделать запрос более специфичным.
+      error({
+        text: 'Too many matches found. Please enter a more specific query!',
+      });
+    } else if (countries.length === 0) {
+      // если вернулся пустой массив
+      error({
+        text: 'Country not found!',
+      });
+    }
+  });
+}
 
 // функция очистки результата поиска перед вводом поиска новой страны
 function clearSearchFormInput() {
